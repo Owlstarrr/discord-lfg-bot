41101502c07745154635ae0f09d014e68a7ca0a3
Laptop <90736801+Owlstarrr@users.noreply.github.com>
2025-08-12 14:15:58 -0700

    finished v1 implementation of LFG message, re-organised methods into Util class

---
+++ src/main/java/org/lfgbot/LfgUtil.java
@@ -0,0 +1,53 @@
+package org.lfgbot;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.channel.middleman.AudioChannel;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+
+import java.awt.*;
+import java.util.concurrent.TimeUnit;
+
+public class LfgUtil {
+
+    public static void handleLfg(String messageAsString, MessageChannel channel, Member messageAuthor, String colorAsHex) {
+
+        // TEMP message for testing purposes
+        channel.sendMessage("LFG Message Recieved, Content: " + messageAsString).queue(sentMessage -> {
+            sentMessage.delete().queueAfter(3, TimeUnit.SECONDS);
+        });
+
+        // IF USER IS IN VC
+        if (messageAuthor != null
+                && messageAuthor.getVoiceState() != null
+                && messageAuthor.getVoiceState().inAudioChannel()) {
+
+            AudioChannel vc = messageAuthor.getVoiceState().getChannel();
+            if (vc == null) throw new NullPointerException("VC Cannot be null");
+
+            //Create invite to channel
+            MessageEmbed embed = buildEmbedForLfg(messageAuthor, messageAsString, vc, colorAsHex);
+            channel.sendMessageEmbeds(embed).queue();
+
+
+            // User is not in a VC
+        } else if (messageAuthor != null) {
+            channel.sendMessage(messageAuthor.getAsMention() +
+                    " you must be in a voice channel to use this command.").queue(sentMessage -> {
+                sentMessage.delete().queueAfter(5, TimeUnit.SECONDS);
+            });
+        }
+    }
+
+    public static MessageEmbed buildEmbedForLfg(Member messageAuthor, String trimmedMsg, AudioChannel vc, String colorAsHex) {
+        EmbedBuilder builder = new EmbedBuilder();
+        builder.setAuthor(messageAuthor.getEffectiveName(),
+                null, messageAuthor.getEffectiveAvatarUrl());
+        builder.setColor(Color.decode(colorAsHex));
+        builder.setTitle("Looking for Group");
+        builder.addField("User message: ", trimmedMsg, false);
+        builder.addField("Join here ", vc.getAsMention(), false);
+        return builder.build();
+    }
+}
--- src/main/java/org/lfgbot/listeners/EventListener.java
+++ src/main/java/org/lfgbot/listeners/EventListener.java
@@ -1,19 +1,11 @@
 package org.lfgbot.listeners;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.*;
-import net.dv8tion.jda.api.entities.channel.Channel;
-import net.dv8tion.jda.api.entities.channel.middleman.AudioChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
-import org.lfgbot.LfgBot;
-
-import java.awt.*;
-import java.util.HexFormat;
-import java.util.concurrent.TimeUnit;
+import org.lfgbot.LfgUtil;
 
 // Event Listeners for Discord Bot
 public class EventListener extends ListenerAdapter {
@@ -46,48 +38,8 @@ public class EventListener extends ListenerAdapter {
         System.out.println("MSG: " + trimmedMessage);
 
         event.getMessage().delete().queue();
-        handleLfg(trimmedMessage, channel, messageSender);
+        LfgUtil.handleLfg(trimmedMessage, channel, messageSender, colorAsHex);
 
     }
-
-    private void handleLfg(String message, MessageChannel channel, Member messageAuthor) {
-
-        // TEMP message for testing purposes
-        channel.sendMessage("LFG Message Recieved, Content: " + message).queue(sentMessage -> {
-            sentMessage.delete().queueAfter(3, TimeUnit.SECONDS);
-        });
-
-        // IF USER IS IN VC
-        if (messageAuthor != null
-                && messageAuthor.getVoiceState() != null
-                && messageAuthor.getVoiceState().inAudioChannel()) {
-
-            AudioChannel vc = messageAuthor.getVoiceState().getChannel();
-
-            //TODO: integrate embed creation
-            //buildEmbedForLfg();
-
-        // User is not in a VC
-        } else if (messageAuthor != null) {
-                channel.sendMessage(messageAuthor.getAsMention() +
-                        " you must be in a voice channel to use this command.").queue(sentMessage -> {
-                            sentMessage.delete().queueAfter(5, TimeUnit.SECONDS);
-                        });
-        }
-    }
-
-    private EmbedBuilder buildEmbedForLfg(Member messageAuthor, MessageChannel channel, Message message, String msgAsStr, AudioChannel vc) {
-        EmbedBuilder builder = new EmbedBuilder();
-
-        builder.setAuthor(messageAuthor.getEffectiveName(),
-                null, messageAuthor.getEffectiveAvatarUrl());
-        builder.setColor(Color.decode(colorAsHex));
-        builder.setTitle(messageAuthor.getAsMention() + " is Looking for Group");
-        builder.setDescription(message.getContentRaw());
-        builder.addField("Join here", "[" + vc.getJumpUrl() + "]", false);
-
-        return builder;
-    }
-
 
 }
